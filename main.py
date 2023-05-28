249299c5b0aa5364dbe1ec96de3c642c35f2f3a1
Mariusz_B_23 <45122048+MariuszBlaszczyk@users.noreply.github.com>
2023-05-28 14:27:12 +0200

    refactor

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
@@ -21,21 +20,19 @@ async def ping(ctx):
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+    await ctx.send("Choo choo! <@953629691691012167>  🚅")
     
-@bot.command()
-async def bbj(ctx):
-    await ctx.send("Raków RKS !!!")
-    
-@bot.command()
-async def ai(ctx, text):
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(os.environ["IP_MAKE"], data={'text': text}) as response:
-                result = await response.text()
-                await ctx.send(f"External IP response: {result}")
-    except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
-    
-
+@bot.command(brief='Podaj mi tekst do przetworzenia', usage='!ai <tekst>')
+async def ai(ctx, *, text: str):
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post('IP_MAKE', data=text) as resp:
+                if resp.status == 200:
+                    response_text = await resp.text()
+                    await ctx.send(response_text)
+                else:
+                    await ctx.send("Wystąpił problem, spróbuj ponownie później.")
+        except:
+            await ctx.send("Wystąpił problem, spróbuj ponownie później.")
+        
 bot.run(os.environ["DISCORD_TOKEN"])
